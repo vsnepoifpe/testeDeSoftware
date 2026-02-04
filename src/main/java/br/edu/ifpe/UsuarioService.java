acdad8a923816d5a9685c469da3c60d5cfe06595
Vilmar Nepomuceno <vilmarnepomuceno@recife.ifpe.edu.br>
2026-02-04 15:16:07 -0300

    Implemetação do teste.

--- src/main/java/br/edu/ifpe/UsuarioService.java
+++ src/main/java/br/edu/ifpe/UsuarioService.java
@@ -2,7 +2,14 @@ package br.edu.ifpe;
 
 public class UsuarioService {
 
+    private UsuarioReporitorio usuarioReporitorio;
+
+    public UsuarioService( UsuarioReporitorio repo) {
+        this.usuarioReporitorio = repo;
+    }
 
     public void cadastrar(Usuario usuario) {
+
+        this.usuarioReporitorio.inserir(usuario);
     }
 }
